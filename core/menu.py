77013ede94bc82c3e0c8a6920fb43aa7dbe09dc0
Anwar212 <anwar212@github.com>
2018-10-03 15:56:15 +0000

    websploit

---
+++ core/menu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# WebSploit FrameWork Menu module
+from core import wcolors
+
+def main_info():
+	ston = wcolors.color.BLUE + "[" + wcolors.color.ENDC
+	print ""
+	print "\t\t--=" + ston + wcolors.color.GREEN + "WebSploit Framework" + wcolors.color.ENDC
+	print "\t+---**---==" + ston + "Version :" + wcolors.color.RED + "3.5.0" + wcolors.color.ENDC
+	print "\t+---**---==" + ston + "Codename :" + wcolors.color.RED + "Anwar" + wcolors.color.ENDC
+	print "\t+---**---==" + ston + "Available Modules : " + wcolors.color.GREEN + "20" + wcolors.color.ENDC
+	print "\t\t--=" + ston + "Update Date : [" + wcolors.color.RED + "r3.3.1-000 20.9.2018" + wcolors.color.ENDC + "]"
+	print "\n\n"
